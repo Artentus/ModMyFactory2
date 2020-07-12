c57ee64566569837ecd18467193fe3b017f72234
Mathis Rech <mathisrech@gmx.de>
2020-07-12 12:29:52 +0200

    Add proper URL encoding for login requests

--- ModMyFactory.WebApi/Authentication.cs
+++ ModMyFactory.WebApi/Authentication.cs
@@ -7,7 +7,6 @@
 
 using Newtonsoft.Json;
 using System.Net;
-using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,41 +28,10 @@ namespace ModMyFactory.WebApi
         /// </summary>
         /// <param name="username">The username to log in.</param>
         /// <param name="password">The password to log in.</param>
-        public async static Task<(string username, string token)> LogInAsync(string username, SecureString password)
-        {
-            string contentString = $"api_version=2&require_game_ownership=true&username={username}&password=";
-            var contentBytes = Encoding.UTF8.GetBytes(contentString);
-            var pwBytes = password.ToBytes();
-
-            var content = new byte[contentBytes.Length + pwBytes.Length];
-            contentBytes.CopyTo(content, 0);
-            pwBytes.CopyTo(content, contentBytes.Length);
-
-            try
-            {
-                string document = await WebHelper.RequestDocumentAsync(LogInUrl, content);
-                dynamic response = JsonConvert.DeserializeObject(document);
-                return (response.username, response.token);
-            }
-            catch (WebException ex)
-            {
-                throw ApiException.FromWebException(ex);
-            }
-            finally
-            {
-                DestroyByteArray(content);
-            }
-        }
-
-        /// <summary>
-        /// Logs into the API to receive a token. The token can be used to authenticate to other API endpoints.
-        /// </summary>
-        /// <param name="username">The username to log in.</param>
-        /// <param name="password">The password to log in.</param>
         public async static Task<(string username, string token)> LogInAsync(string username, string password)
         {
             string contentString = $"api_version=2&require_game_ownership=true&username={username}&password={password}";
-            var content = Encoding.UTF8.GetBytes(contentString);
+            var content = Encoding.UTF8.GetBytes(WebUtility.UrlEncode(contentString));
 
             try
             {
