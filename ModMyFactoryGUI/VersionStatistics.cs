a8e1afbb14249b90a3fecc4c563c0502ce5fc16b
Mathis Rech <mathisrech@gmx.de>
2020-09-23 17:17:56 +0200

    Fix compile error

--- ModMyFactoryGUI/VersionStatistics.cs
+++ ModMyFactoryGUI/VersionStatistics.cs
@@ -20,6 +20,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
+#if NETCORE && SELFCONTAINED
+using System.Runtime.InteropServices;
+#endif
+
 namespace ModMyFactoryGUI
 {
     internal static class VersionStatistics
